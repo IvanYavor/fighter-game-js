80d930a732de6008290701941552577cc5d23f38
Ivan Yavorsky <02ivan0971@gmail.com>
2021-01-17 21:51:16 +0200

    Implement getHitPower, getBlockPower, getDemage methods

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -8,12 +8,17 @@ export async function fight(firstFighter, secondFighter) {
 
 export function getDamage(attacker, defender) {
   // return damage
+  const demage = getHitPower(attacker) - getBlockPower(defender);
+  return demage > 0 ? demage : 0;
 }
 
 export function getHitPower(fighter) {
   // return hit power
+  const criticalChance = fighter.criticalInput === 3 ? 2 : Math.random() + 1;
+  return fighter.attack * criticalChance;
 }
 
 export function getBlockPower(fighter) {
   // return block power
+  return fighter.defence * Math.random() + 1;
 }
